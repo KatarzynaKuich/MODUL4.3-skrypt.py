ffb9202497d3c8bfe1ddbac68e6a3c9b3fdc2167
Katarzyna Kuich <89656991+KatarzynaKuich@users.noreply.github.com>
2021-10-27 20:46:23 +0200

    2 commit

--- skrypt.py
+++ skrypt.py
@@ -1,1 +1,12 @@
 print("Greetings from file!")
+#Nazwij go skrypt.py i uruchom go tą komendą
+
+#python skrypt.py
+#Widzisz tekst na konsoli? Jeśli tak, to świetnie!
+
+#Ten plik zachowuje się dokładnie tak samo, jak uruchamiany w edytorze. Możemy więc tworzyć w nim funkcje i je wywoływać. Przekonaj się, wpisując ten kod:
+
+def greeting_from_file():
+    print("Greetings from file!")
+
+greeting_from_file()
